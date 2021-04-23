62c33d7813e034e45bfda430b2aaa3b36aeaa25d
Antozzi <antondyrdin@gmail.com>
2021-04-23 20:50:29 +0500

    HW4

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Nothing is in here....");
+
+        ABCPrinter abcPrinter = new ABCPrinter();
+        Thread t1 = new Thread(abcPrinter::printA);
+        Thread t2 = new Thread(abcPrinter::printB);
+        Thread t3 = new Thread(abcPrinter::printC);
+        t1.start();
+        t2.start();
+        t3.start();
+
     }
 }
+
