08b1ac15f9b875f7c9ab2b0e4bc390810e487fb5
Antozzi <antondyrdin@gmail.com>
2021-05-05 09:28:07 +0500

    HW6

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,6 +1,13 @@
+import test.After4Test1;
+import test.After4Test2;
+import test.Only1and4Test;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Nothing is in here....");
+        new After4Test1();
+        new After4Test2(new int[]{1, 6, 5, 4, 2, 3, 3},new int[]{4, 3, 7, 5, 4, 4, 3, 2});
+        new Only1and4Test(new int[]{1, 1, 4, 4, 4, 1, 4, 1}, true);
+
     }
 }
